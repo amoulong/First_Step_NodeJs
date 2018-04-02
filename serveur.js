4ec5897f780bdf2337f53334517f0e2c35d42abd
amoulong <moulongarmel@yahoo.fr>
2018-04-02 09:19:48 +0200

    Add files via upload

---
+++ serveur.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+
+var server = http.createServer(function(req, res) {
+  var params = querystring.parse(url.parse(req.url).query);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  var page = url.parse(req.url).pathname;
+  console.log(page);
+  if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Vous devez bien avoir un prenom et un nom, non ?');
+    }
+  res.end();
+});
+server.listen(8080);
